2d1993441710af47f1b8a95f86f0127583545b85
Endy Suryo Laksono <endysuryalaksana@gmail.com>
2022-11-12 10:37:38 +0700

    scaffold project and slicing search page

---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="bg-gradient-to-br from-[#712bda] to-[#a45deb]">
+    <div
+      class="px-8 py-7 min-h-screen flex flex-col justify-between md:justify-center items-center w-full md:w-4/12 md:mx-auto"
+    >
+      <section class="my-auto md:my-10">
+        <img
+          src="/images/logo.webp"
+          width="73"
+          height="85"
+          alt="Logo"
+          class="md:w-44 md:h-auto"
+        />
+      </section>
+      <div class="flex flex-col gap-4 w-full">
+        <input
+          v-model="title"
+          name="title"
+          type="text"
+          class="py-3 rounded-full flex flex-col text-center placeholder:text-[#64748b]"
+          placeholder="Artist / Album / Title"
+        />
+        <button
+          class="py-3 rounded-full text-white bg-white/20 disabled:bg-white/50"
+          :disabled="isDisable"
+          @click="search"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type IData = {
+  title: string | undefined
+}
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data(): IData {
+    return {
+      title: 'Jason Mraz',
+    }
+  },
+  computed: {
+    isDisable() {
+      if (this.title === '') {
+        return true
+      }
+
+      return false
+    },
+  },
+  methods: {
+    search() {
+      // eslint-disable-next-line no-console
+      console.log('heheehh', this.title)
+      this.$router.push({
+        name: 'result',
+        query: {
+          title: this.title,
+        },
+      })
+    },
+  },
+})
+</script>
---
+++ pages/result.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg-[#f8fafc] min-h-screen">
+    <nav
+      class="flex flex-row justify-between items-center sticky top-0 bg-gradient-to-r from-[#712bda] to-[#a45deb] w-full p-4 shadow-md"
+    >
+      <img src="/images/menu.webp" width="14" height="auto" alt="menu" />
+      <img src="/images/ngmusic.webp" width="72" height="auto" alt="menu" />
+      <img src="/images/search.webp" width="14" height="auto" alt="menu" />
+    </nav>
+    <div class="flex flex-row justify-center items-center gap-4 py-10">
+      <div class="text-[#334155]">Search result for :</div>
+      <div class="text-[#7b34dd] text-lg font-bold">Json Mraz</div>
+    </div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+    <div class="text-6xl">Hehehe</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Result',
+})
+</script>
